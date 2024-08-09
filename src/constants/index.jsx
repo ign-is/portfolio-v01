9cc9210b50a75fcac2078f93717e16d4ef5e7c9f
ign-is <igna.dev.contact@gmail.com>
2024-08-09 11:44:15 -0300

    Add constants

---
+++ src/constants/index.jsx
@@ -0,0 +1,160 @@
+import advice from '../assets/advice.jpg'
+import anime from '../assets/anime-min.jpg'
+import blogr from '../assets/blogr.jpg'
+// import brainOne from '../assets/brain-1.jpg'
+import brain from '../assets/brain-min.jpg'
+import clock from '../assets/clock.jpg'
+import coffe from '../assets/cofe.jpg'
+import domTodo from '../assets/dom-todo.jpg'
+import easybank from '../assets/easybank.jpg'
+import expenses from '../assets/expenses.jpg'
+import flexBox from '../assets/flexbox-min.jpg'
+import formSpace from '../assets/form1-min.jpg'
+import iCodeThis from '../assets/icode-min.jpg'
+import news from '../assets/news.jpg'
+import newsletter from '../assets/newsletter-mobile.jpg'
+import nft from '../assets/nft.jpg'
+import nikeThree from '../assets/nike-3-min.jpg'
+import product from '../assets/product.jpg'
+import qr from '../assets/qr.jpg'
+import randomColor from '../assets/random-color.jpg'
+import randomQuotes from '../assets/random-quotes.jpg'
+import rangeColor from '../assets/range-color-rgb.jpg'
+import restaurantMin from '../assets/restaurant-min.jpg'
+import result from '../assets/result-progres.jpg'
+import room from '../assets/room.jpg'
+import socialProof from '../assets/social-proof.jpg'
+import social from '../assets/social.jpg'
+import stats from '../assets/stats.jpg'
+import timeTracker from '../assets/time-tracker.jpg'
+import travelOne from '../assets/travel-1-min.jpg'
+import tribu from '../assets/tribut-min.jpg'
+import vercel from '../assets/vercel.jpg'
+
+export const PROJECTS = [
+    {
+        image: advice,
+        technologies: ['React']
+    },
+    {
+        image: anime,
+        technologies: ['Next.js', 'Tailwind', 'Framer Motion']
+    },
+    {
+        image: blogr,
+        technologies: ['HTML', 'CSS', 'JavaScript']
+    },
+    {
+        image: brain,
+        technologies: ['React', 'Tailwind']
+    },
+    {
+        image: clock,
+        technologies: ['HTML', 'CSS', 'JavaScript']
+    },
+    {
+        image: coffe,
+        technologies: ['HTML', 'CSS']
+    },
+    {
+        image: domTodo,
+        technologies: ['HTML', 'CSS', 'JavaScript']
+    },
+    {
+        image: easybank,
+        technologies: ['React']
+    },
+    {
+        image: expenses,
+        technologies: ['HTML', 'CSS', 'JavaScript']
+    },
+    {
+        image: flexBox,
+        technologies: ['HTML', 'CSS']
+    },
+    {
+        image: formSpace,
+        technologies: ['HTML', 'CSS']
+    },
+    {
+        image: iCodeThis,
+        technologies: ['HTML', 'CSS', 'JavaScript']
+    },
+    {
+        image: news,
+        technologies: ['HTML', 'CSS', 'JavaScript']
+    },
+    {
+        image: newsletter,
+        technologies: ['HTML', 'CSS', 'JavaScript']
+    },
+    {
+        image: nft,
+        technologies: ['HTML', 'CSS']
+    },
+    {
+        image: nikeThree,
+        technologies: ['React', 'Tailwind']
+    },
+    {
+        image: product,
+        technologies: ['HTML', 'CSS']
+    },
+    {
+        image: qr,
+        technologies: ['HTML', 'CSS']
+    },
+    {
+        image: randomColor,
+        technologies: ['HTML', 'CSS', 'JavaScript']
+    },
+    {
+        image: randomQuotes,
+        technologies: ['HTML', 'CSS', 'JavaScript']
+    },
+    {
+        image: rangeColor,
+        technologies: ['HTML', 'CSS', 'JavaScript']
+    },
+    {
+        image: restaurantMin,
+        technologies: ['React', 'Tailwind', 'Framer Motion']
+    },
+    {
+        image: result,
+        technologies: ['HTML', 'CSS', 'JavaScript']
+    },
+    {
+        image: room,
+        technologies: ['React']
+    },
+    {
+        image: socialProof,
+        technologies: ['HTML', 'CSS']
+    },
+    {
+        image: social,
+        technologies: ['HTML', 'CSS', 'JavaScript']
+    },
+    {
+        image: stats,
+        technologies: ['HTML', 'CSS']
+    },
+    {
+        image: timeTracker,
+        technologies: ['React']
+    },
+    {
+        image: travelOne,
+        technologies: ['Next.js', 'Tailwind']
+    },
+    {
+        image: tribu,
+        technologies: ['HTML', 'CSS']
+    },
+    {
+        image: vercel,
+        technologies: ['Next.js','TypeScript']
+    }
+]
+
