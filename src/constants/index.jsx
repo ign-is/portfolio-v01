874f79c893e1a09248c7dd22232ca9b3fafe2406
ign-is <igna.dev.contact@gmail.com>
2024-08-13 15:42:31 -0300

    Add github links to constants

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,7 +1,6 @@
 import advice from '../assets/advice.jpg'
 import anime from '../assets/anime-min.jpg'
 import blogr from '../assets/blogr.jpg'
-// import brainOne from '../assets/brain-1.jpg'
 import brain from '../assets/brain-min.jpg'
 import clock from '../assets/clock.jpg'
 import coffe from '../assets/cofe.jpg'
@@ -33,190 +32,221 @@ import vercel from '../assets/vercel.jpg'
 
 export const PROJECTS = [
     {
+        image: iCodeThis,
+        technologies: ['HTML', 'CSS', 'JavaScript'],
+        description: 'Portfolio with 166 front-end completed challenges!',
+        title: '166 Front-end Challenges',
+        github: 'https://icodethis.com/igna'
+    },
+    {
         image: advice,
         technologies: ['React'],
         description: 'Click the dice to generate a new advice.',
-        title: 'Advice generator app'
+        title: 'Advice generator app',
+        github: 'https://github.com/ign-is/frontend-mentor/tree/main/advice-generator-app'
     },
     {
         image: anime,
         technologies: ['Next.js', 'Tailwind', 'Framer Motion'],
         description: 'Modern Next 14 server side app to explore animes with server actions, infinite scroll & Framer Motion animations.',
-        title: 'Best Animes'
+        title: 'Best Animes',
+        github: ''
     },
     {
         image: blogr,
         technologies: ['HTML', 'CSS', 'JavaScript'],
         description: 'A modern blog publishing platform.',
-        title: 'Blogr landing page'
+        title: 'Blogr landing page',
+        github: 'https://github.com/ign-is/frontend-mentor/tree/main/blogr-landing-page'
     },
     {
         image: brain,
         technologies: ['React', 'Tailwind'],
         description: 'Futuristic fully responsive website with modern UI/UX, parallax effects and bento box layouts.',
-        title: 'Brainwave'
+        title: 'Brainwave',
+        github: 'https://github.com/ign-is/brainwave'
     },
     {
         image: clock,
         technologies: ['HTML', 'CSS', 'JavaScript'],
         description: 'Digital clock in Neumorphism design style with dark mode included.',
-        title: 'Neumorphism Clock'
+        title: 'Neumorphism Clock',
+        github: 'https://github.com/ign-is/neumorphism-digital-clock'
     },
     {
         image: coffe,
         technologies: ['HTML', 'CSS'],
         description: 'Coffee shop menu with a touch of CSS art.',
-        title: 'Coffee Menu'
+        title: 'Coffee Menu',
+        github: 'https://github.com/ign-is/coffee-shop-menu'
     },
     {
         image: domTodo,
         technologies: ['HTML', 'CSS', 'JavaScript'],
         description: 'Photorealistic to-do device.',
-        title: 'DOM To-do'
+        title: 'DOM To-do',
+        github: 'https://github.com/ign-is/javascript-dom-manipulation/tree/main/DOM-todo'
     },
     {
         image: easybank,
         technologies: ['React'],
         description: 'Next generation digital banking.',
-        title: 'Easybank landing page'
+        title: 'Easybank landing page',
+        github: 'https://github.com/ign-is/frontend-mentor/tree/main/easybank-landing-page'
     },
     {
         image: expenses,
         technologies: ['HTML', 'CSS', 'JavaScript'],
         description: 'Cool chart to visualize and track expenses.',
-        title: 'Expenses chart component'
+        title: 'Expenses chart component',
+        github: 'https://github.com/ign-is/frontend-mentor/tree/main/expenses-chart-component'
     },
     {
         image: flexBox,
         technologies: ['HTML', 'CSS'],
         description: 'Handy technical documentation page filled with custom CSS made graphical examples to understand Flexbox properties.',
-        title: 'Flexbox Documentation'
+        title: 'Flexbox Documentation',
+        github: 'https://github.com/ign-is/flexbox-technical-documentation'
     },
     {
         image: formSpace,
         technologies: ['HTML', 'CSS'],
         description: 'Survey form for a space tourism company.',
-        title: 'Space Travel Form'
-    },
-    {
-        image: iCodeThis,
-        technologies: ['HTML', 'CSS', 'JavaScript'],
-        description: 'Portfolio with 166 front-end completed challenges!',
-        title: '166 Front-end Challenges'
+        title: 'Space Travel Form',
+        github: 'https://github.com/ign-is/space-survey-form'
     },
     {
         image: news,
         technologies: ['HTML', 'CSS', 'JavaScript'],
         description: 'Find the latest technological news on this website.',
-        title: 'News homepage'
+        title: 'News homepage',
+        github: 'https://github.com/ign-is/frontend-mentor/tree/main/news-homepage'
     },
     {
         image: newsletter,
         technologies: ['HTML', 'CSS', 'JavaScript'],
         description: 'Sign-up form with validation and success message.',
-        title: 'Newsletter sign-up form'
+        title: 'Newsletter sign-up form',
+        github: 'https://github.com/ign-is/frontend-mentor/tree/main/newsletter-sign-up-with-success-message'
     },
     {
         image: nft,
         technologies: ['HTML', 'CSS'],
         description: 'Card component showcasing NFT futuristic art.',
-        title: 'NFT preview card'
+        title: 'NFT preview card',
+        github: 'https://github.com/ign-is/frontend-mentor/tree/main/nft-preview-card-component'
     },
     {
         image: nikeThree,
         technologies: ['React', 'Tailwind'],
         description: 'Nike landing page showcasing products.',
-        title: 'Nike landing'
+        title: 'Nike landing',
+        github: 'https://github.com/ign-is/nike'
     },
     {
         image: product,
         technologies: ['HTML', 'CSS'],
         description: 'Card showcasing perfume with price and add to cart button.',
-        title: 'Product preview card'
+        title: 'Product preview card',
+        github: 'https://github.com/ign-is/frontend-mentor/tree/main/product-preview-card-component'
     },
     {
         image: qr,
         technologies: ['HTML', 'CSS'],
         description: 'Simple card component with QR.',
-        title: 'QR code'
+        title: 'QR code',
+        github: 'https://github.com/ign-is/frontend-mentor/tree/main/qr-code-component'
     },
     {
         image: randomColor,
         technologies: ['HTML', 'CSS', 'JavaScript'],
         description: 'Random hexadecimal color generator with a cool button.',
-        title: 'Random color generator'
+        title: 'Random color generator',
+        github: 'https://github.com/ign-is/javascript-dom-manipulation/tree/main/random-color'
     },
     {
         image: randomQuotes,
         technologies: ['HTML', 'CSS', 'JavaScript'],
         description: 'Stoic philosophers random quotes generator with a beautiful background architecture.',
-        title: 'Quotes generator'
+        title: 'Quotes generator',
+        github: 'https://github.com/ign-is/javascript-dom-manipulation/tree/main/random-quotes'
     },
     {
         image: rangeColor,
         technologies: ['HTML', 'CSS', 'JavaScript'],
         description: 'Photorealistic CSS Art with RGB range color generator functionality.',
-        title: 'RGB color picker'
+        title: 'RGB color picker',
+        github: 'https://github.com/ign-is/javascript-dom-manipulation/tree/main/range-color-rgb'
     },
     {
         image: restaurantMin,
         technologies: ['React', 'Tailwind', 'Framer Motion'],
         description: 'Modern restaurant landing page.',
-        title: 'Restaurant'
+        title: 'Restaurant',
+        github: 'https://github.com/ign-is/restaurant-landing-page'
     },
     {
         image: result,
         technologies: ['HTML', 'CSS', 'JavaScript'],
         description: 'View results with cool hover effects.',
-        title: 'Results summary component'
+        title: 'Results summary component',
+        github: 'https://github.com/ign-is/frontend-mentor/tree/main/results-summary-component'
     },
     {
         image: room,
         technologies: ['React'],
         description: 'Find modern ways to decorate your home with this responsive slider.',
-        title: 'Room homepage'
+        title: 'Room homepage',
+        github: 'https://github.com/ign-is/frontend-mentor/tree/main/room-homepage'
     },
     {
         image: socialProof,
         technologies: ['HTML', 'CSS'],
         description: 'Fully responsive social proof rating and comments section.',
-        title: 'Social proof section'
+        title: 'Social proof section',
+        github: 'https://github.com/ign-is/frontend-mentor/tree/main/social-proof-section'
     },
     {
         image: social,
         technologies: ['HTML', 'CSS', 'JavaScript'],
         description: 'Followers count dashboard with theme switcher.',
-        title: 'Social Media Dashboard'
+        title: 'Social Media Dashboard',
+        github: 'https://github.com/ign-is/frontend-mentor/tree/main/social-media-dashboard-with-theme-switcher'
     },
     {
         image: stats,
         technologies: ['HTML', 'CSS'],
         description: 'Responsive stats card.',
-        title: 'Stats Preview Card'
+        title: 'Stats Preview Card',
+        github: 'https://github.com/ign-is/frontend-mentor/tree/main/stats-preview-card-component'
     },
     {
         image: timeTracker,
         technologies: ['React'],
         description: 'Organize your time with daily, weekly, and monthly stats.',
-        title: 'Time tracking dashboard'
+        title: 'Time tracking dashboard',
+        github: 'https://github.com/ign-is/frontend-mentor/tree/main/time-tracking-dashboard'
     },
     {
         image: travelOne,
         technologies: ['Next.js', 'Tailwind', 'TypeScript'],
         description: 'Fully responsive travel and camping landing page.',
-        title: ' Travel App'
+        title: ' Travel App',
+        github: 'https://github.com/ign-is/travel-landing-page'
     },
     {
         image: tribu,
         technologies: ['HTML', 'CSS'],
         description: 'Marcus Aurelius tribute page.',
-        title: 'Tribute Page'
+        title: 'Tribute Page',
+        github: 'https://github.com/ign-is/tribute-page'
     },
     {
         image: vercel,
         technologies: ['Next.js','TypeScript'],
         description: 'Full-stack dashboard web application form the official Next.js documentation.',
-        title: 'Vercel Official Project'
+        title: 'Next.js Official Dashboard',
+        github: 'https://github.com/ign-is/nextjs-dashboard'
     }
 ]
 
