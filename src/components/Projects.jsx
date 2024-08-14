b38e1713134968889b2428290a1a0ca8607a38b2
ign-is <igna.dev.contact@gmail.com>
2024-08-14 20:45:48 -0300

    Add fix Project prop

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,10 +4,10 @@ import { PROJECTS } from "../constants";
 const Projects = () => {
     return (
         <section id="projects">
-           <h1>Projects</h1>
+           {/* <h1>Projects</h1> */}
             <div className="container-blue">
                 {PROJECTS.map((project, index) => (
-                    <ProjectCard key={index} image={project.image} />
+                    <ProjectCard key={index} project={project} />
                 ))}
             </div>
         </section>
