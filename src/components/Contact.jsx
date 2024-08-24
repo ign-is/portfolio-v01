4a8180bac1fd24a506c0f5b79fb560cafa47665d
ign-is <igna.dev.contact@gmail.com>
2024-08-24 20:22:20 -0300

    Add some details to form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -30,50 +30,67 @@ const Contact = () => {
     return (
         <section id="contact">
             <h1 className="contact-header">CONTACT</h1>
-            <form 
-                ref={formRef}
-                onSubmit={handleSubmit}
-            >
-                <label htmlFor="name">
-                    <input 
-                        type="text" 
-                        name="name"
-                        id="name"
-                        value={form.name}
-                        onChange={handleChange}
-                        placeholder="NAME"    
-                    />
-                </label>
-                <label htmlFor="name">
-                    <input 
-                        type="email" 
-                        name="email"
-                        id="email"
-                        value={form.email}
-                        onChange={handleChange}
-                        placeholder="EMAIL"    
-                    />
-                </label>
-                <label htmlFor="message">
-                    <textarea 
-                        name="message" 
-                        id="message"
-                        rows={7}
-                        cols={38}
-                        value={form.message}
-                        onChange={handleChange}
-                        placeholder="LEAVE ME A MESSAGE"    
-                    >
-                    </textarea>
-                </label>
-
-                <button
-                    type="submit"
-                    className="submit"
+            <div className="form-container">
+                <div className="shape-top-right">
+                    <div className="shape-top-top"></div>
+                    <div className="shape-top-left"></div>
+                    <div className="shape-top-right-line"></div>
+                </div>
+                <div className="left-bar">
+                    <div className="triangle-left-top"></div>
+                    <div className="triangle-left-bottom"></div>
+                </div>
+                <div className="right-bar">
+                    <div className="triangle-right-top"></div>
+                    <div className="triangle-right-bottom"></div>
+                </div>
+                <form 
+                    ref={formRef}
+                    onSubmit={handleSubmit}
                 >
-                    {loading ? "Sending..." : "Send"}
-                </button>
-            </form>
+                    <label htmlFor="name">
+                        <input 
+                            type="text" 
+                            name="name"
+                            id="name"
+                            value={form.name}
+                            onChange={handleChange}
+                            placeholder="NAME"    
+                        />
+                    </label>
+                    <label htmlFor="name">
+                        <input 
+                            type="email" 
+                            name="email"
+                            id="email"
+                            value={form.email}
+                            onChange={handleChange}
+                            placeholder="EMAIL"    
+                        />
+                    </label>
+                    <label htmlFor="message">
+                        <textarea 
+                            name="message" 
+                            id="message"
+                            rows={7}
+                            cols={38}
+                            value={form.message}
+                            onChange={handleChange}
+                            placeholder="LEAVE A MESSAGE"    
+                        >
+                        </textarea>
+                    </label>
+
+                    <button
+                        type="submit"
+                        className="submit"
+                    >
+                        <span>
+                            {loading ? "SENDING..." : "SEND"}
+                        </span>
+                    </button>
+                </form>
+            </div>
         </section>
     )
 }
